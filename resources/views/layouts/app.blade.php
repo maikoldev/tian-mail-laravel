ea6923ecba6b28419afcb68e93b3ae02b6812e2e
Jhon Michael Garcia <me@maikoldev.com>
2023-04-25 14:46:28 -0500

    Se añade titulo al Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,24 +3,14 @@
 
 <head>
     <meta charset="utf-8">
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    >
-
-    <title>Laravel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,600"
-        rel="stylesheet"
-    >
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <!-- Scripts -->
-    <script
-        src="{{ asset('js/app.js') }}"
-        defer
-    ></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
