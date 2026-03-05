0ddd6965f98d4b07cc569781c266cd562b268122
Jhon Michael Garcia <jmgarciac@pm.me>
2026-03-04 20:32:30 -0500

    Upgrade Laravel 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,8 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        //
+        // Laravel 10 registers policies automatically.
     }
 }
