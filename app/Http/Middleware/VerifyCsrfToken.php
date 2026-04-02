8b661a5f26d612f037cc68a816508d94b68484c4
Jhon Michael Garcia <jmgarciac@pm.me>
2026-04-01 19:05:02 -0500

    CSRF Except

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/certificates/generate',
+        'certificates/generate',
+        '*/certificates/generate',
     ];
 }
